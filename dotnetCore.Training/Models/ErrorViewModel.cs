20f71f4b8f4b1cc758d1869e6728dc3a2bcf4470
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-07-24 12:44:51 +0530

    dotnet_core 1

---
+++ dotnetCore.Training/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnetCore.Training.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ dotnetCore.Training/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace dotnetCore.Training.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
