1cff0f8a2149b6f8dd3bd37b8ae268dd935aa6d4
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-07-30 14:18:17 +0530

    dotnetCore Session 3

---
+++ dotnetCore.Training/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using dotnetCore.Training.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetCore.Training.Data
+{
+    //Database
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        //Tables
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- dotnetCore.Training/Program.cs
+++ dotnetCore.Training/Program.cs
@@ -1,8 +1,17 @@
+using dotnetCore.Training.Data;
+using Microsoft.EntityFrameworkCore;
+
 //DI Container
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.
+    AddDbContext<ApplicationDbContext>
+    (options=>options.UseSqlServer
+    (builder.Configuration.
+    GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
