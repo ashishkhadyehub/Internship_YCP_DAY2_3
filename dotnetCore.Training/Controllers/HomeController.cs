2dcc981a5fa7d160f7f4c0caf3e840ce4a2f094c
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-08-01 14:20:37 +0530

    dotnetCore session 4

--- dotnetCore.Training/Controllers/HomeController.cs
+++ dotnetCore.Training/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using dotnetCore.Training.Data;
 using dotnetCore.Training.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,6 +9,7 @@ namespace dotnetCore.Training.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
---
+++ dotnetCore.Training/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using dotnetCore.Training.Data;
+using dotnetCore.Training.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetCore.Training.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        //select field and press [ctrl + .] to generate constructor
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var studentList = _context.Students.ToList();
+            return View(studentList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
